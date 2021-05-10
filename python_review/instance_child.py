e035fbf0151d1a6d928a444d6b4eb92f465e6782
tanjiahao <506000311@qq.com>
2021-05-10 23:39:37 +0800

    python_review

---
+++ python_review/instance_child.py
@@ -0,0 +1,4 @@
+from python_review.python_instance import Mytest
+
+t3 = Mytest(object)
+print(t3.name)
---
+++ python_review/python_instance.py
@@ -0,0 +1,23 @@
+
+class Mytest(object):
+
+    __instance = None
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__instance = object.__new__(cls)
+            return cls.__instance
+        else:
+            return cls.__instance
+
+
+
+
+t1 = Mytest()
+t1.name = 'tanjiahao'
+if __name__ == '__main__':
+    print(t1.name)
+    t2 = Mytest()
+    # t2.name = 'toby'
+    print(t2.name)
+    print(id(t1))
+    print(id(t2))
